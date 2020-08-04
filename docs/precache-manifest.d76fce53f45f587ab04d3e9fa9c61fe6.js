80c87c60d0fb735dac843a475807e11903724b47
SerchPro <proyects_serch@hotmail.com>
2020-08-04 17:23:53 -0500

    docs agregado

---
+++ docs/precache-manifest.d76fce53f45f587ab04d3e9fa9c61fe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37bc4ebbb538c7e59142890d0bf08445",
+    "url": "/index.html"
+  },
+  {
+    "revision": "878d7cdf17404ae6be17",
+    "url": "/static/css/main.8c5858ed.chunk.css"
+  },
+  {
+    "revision": "8e2487bde7b016b609b6",
+    "url": "/static/js/2.5fb64923.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5fb64923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878d7cdf17404ae6be17",
+    "url": "/static/js/main.552d8106.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
